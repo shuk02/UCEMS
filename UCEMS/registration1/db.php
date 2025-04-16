4852b7567020d4e6b4a213078a8478f7b1359138
shuk02 <110524815+shuk02@users.noreply.github.com>
2025-04-17 00:04:07 +0800

    Add files via upload

---
+++ UCEMS/registration1/db.php
@@ -0,0 +1,10 @@
+<?php 
+	$server="localhost";
+	$lecturers="root";
+	$password="";
+	$database="ucems";
+	$con=mysqli_connect($server,$lecturers,$password,$database);
+	if(!$con)
+		echo 'Connection failed !';
+?>
+	
